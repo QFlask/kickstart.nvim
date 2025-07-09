84039fb14fa3cea79944f59ffec94e694946c981
QFlask <quinn@flachman.net>
2025-07-08 19:00:02 -0600

    Changed flash colors from this discussion: https://github.com/LazyVim/LazyVim/discussions/1313jk

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -1,8 +1,31 @@
+local function setupCustomHighlightGroup()
+  vim.api.nvim_command 'hi clear FlashMatch'
+  vim.api.nvim_command 'hi clear FlashCurrent'
+  vim.api.nvim_command 'hi clear FlashLabel'
+
+  vim.api.nvim_command 'hi FlashMatch guibg=#4A47A3 guifg=#B8B5FF' -- Emerald background
+  vim.api.nvim_command 'hi FlashCurrent guibg=#456268 guifg=#D0E8F2'
+  vim.api.nvim_command 'hi FlashLabel guibg=#A25772 guifg=#EEF5FF'
+end
+
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
   ---@type Flash.Config
-  opts = {},
+  opts = {
+    highlight = {
+      backdrop = true,
+      groups = {
+        match = 'FlashMatch',
+        current = 'FlashCurrent',
+        backdrop = 'FlashBackdrop',
+        label = 'FlashLabel',
+      },
+    },
+  },
+  config = function()
+    setupCustomHighlightGroup()
+  end,
 	-- stylua: ignore
 	keys = {
 		{ "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
